d4992f942d8cd552f346f064313ba5b269bd9da1
Sam <19FTT1067@student.edu.bn>
2021-09-24 12:16:15 +0800

    new code for temp sensor

--- tempsensor.py
+++ tempsensor.py
@@ -1,16 +1,47 @@
-# import libraries for sensor
+# import libraries
+import glob
 import time
-from w1thermsensor import W1ThermSensor
+import os
+import sys
+import RPi.GPIO as GPIO
 
-# store connection
-sensor = W1ThermSensor()
+#  An Example Reading from /sys/bus/w1/devices/<ds18b20-id>/w1_slave
+#  a6 01 4b 46 7f ff 0c 10 5c : crc=5c YES
+#  a6 01 4b 46 7f ff 0c 10 5c t=26375
 
-# to get the temperature sensor data every second
+#  Set Pullup mode on GPIO14 first.
+GPIO_PIN_NUMBER = 14
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO_PIN_NUMBER, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+def ds18b20_read_sensors():
+  rtn = {}
+  w1_devices = []
+  w1_devices = os.listdir("/sys/bus/w1/devices/")
+  for deviceid in w1_devices:
+    rtn[deviceid] = {}
+    rtn[deviceid]['temp_c'] = None
+    device_data_file = "/sys/bus/w1/devices/" + deviceid + "/w1_slave"
+    if os.path.isfile(device_data_file):
+      try:
+         f = open(device_data_file, "r")
+         data = f.read()
+         f.close()
+         if "YES" in data:
+           (discard, sep, reading) = data.partition(' t=')
+           rtn[deviceid]['temp_c'] = float(reading) / float(1000.0)
+         else:
+           rtn[deviceid]['error'] = 'No YES flag: bad data.'
+      except Exception as e:
+         rtn[deviceid]['error'] = 'Exception during file parsing: ' + str(e)
+    else:
+      rtn[deviceid]['error'] = 'w1_slave file not found.'
+  return rtn;
+
 while True:
-    temperature = sensor.get_temperature()
+  temp_readings = ds18b20_read_sensors()
+  for t in temp_readings:
+    if not 'error' in temp_readings[t]:
+      print(u"Device id '%s' reads %.3f +/- 0.5 °C" % (t, temp_readings[t]['temp_c']))
+  time.sleep(1.0)
 
-    # format temperature data to be printed from an float to a string
-    print("The temperature is %s celsius" % temperature)
-
-    # wait for 1 second between taking temperature reading
-    time.sleep(1)
