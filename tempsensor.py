fcd965657702ab5af78ad9a9883135bb1e4ccb6b
Sam <19FTT1067@student.edu.bn>
2021-09-26 10:17:56 +0800

    create changes for the temp sensor

--- tempsensor.py
+++ tempsensor.py
@@ -1,47 +1,38 @@
 # import libraries
-import glob
-import time
+#!/usr/bin/env python
 import os
-import sys
-import RPi.GPIO as GPIO
+import time
+from time import sleep
 
-#  An Example Reading from /sys/bus/w1/devices/<ds18b20-id>/w1_slave
-#  a6 01 4b 46 7f ff 0c 10 5c : crc=5c YES
-#  a6 01 4b 46 7f ff 0c 10 5c t=26375
+# Load drivers
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
-#  Set Pullup mode on GPIO14 first.
-GPIO_PIN_NUMBER = 14
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(GPIO_PIN_NUMBER, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+# define the location for the sensor output is located
+temp_sensor = '/sys/bus/w1/devices/Sensor_name/w1_slave'
 
-def ds18b20_read_sensors():
-  rtn = {}
-  w1_devices = []
-  w1_devices = os.listdir("/sys/bus/w1/devices/")
-  for deviceid in w1_devices:
-    rtn[deviceid] = {}
-    rtn[deviceid]['temp_c'] = None
-    device_data_file = "/sys/bus/w1/devices/" + deviceid + "/w1_slave"
-    if os.path.isfile(device_data_file):
-      try:
-         f = open(device_data_file, "r")
-         data = f.read()
-         f.close()
-         if "YES" in data:
-           (discard, sep, reading) = data.partition(' t=')
-           rtn[deviceid]['temp_c'] = float(reading) / float(1000.0)
-         else:
-           rtn[deviceid]['error'] = 'No YES flag: bad data.'
-      except Exception as e:
-         rtn[deviceid]['error'] = 'Exception during file parsing: ' + str(e)
-    else:
-      rtn[deviceid]['error'] = 'w1_slave file not found.'
-  return rtn;
+# function to read sensor output file
+def read_temp_raw():
+    f = open(temp_sensor, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
 
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        sleep(0.2)
+        lines = read_temp_raw()
+
+    temp_result = lines[1].find('t=')
+
+    if temp_result != -1:
+        temp_string = lines[1].strip()[temp_result + 2:]
+        # Temperature in Celcius
+        temp = float(temp_string) / 1000.0
+        return temp
+
+# create loop to print out the data every 2 seconds
 while True:
-  temp_readings = ds18b20_read_sensors()
-  for t in temp_readings:
-    if not 'error' in temp_readings[t]:
-      print(u"Device id '%s' reads %.3f +/- 0.5 °C" % (t, temp_readings[t]['temp_c']))
-  time.sleep(1.0)
-
+    print(read_temp())
+    time.sleep(2)
