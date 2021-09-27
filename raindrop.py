62d979cc0fd47f0eef1ad3e2c815e3ffeade7c22
HisyamJasnin <78057234+HisyamJasnin@users.noreply.github.com>
2021-09-26 20:22:38 -0900

    Update raindrop.py
    add some changes to the code

--- raindrop.py
+++ raindrop.py
@@ -1,18 +1,35 @@
 # !/usr/bin/python
 # import libraries
-from time import sleep
+import time
 import os
 import RPi.GPIO as GPIO
 
-pin_rain= 24
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin_rain, GPIO.IN)
+# set GPIO pins
+pin_rain = 22
 
-try :
-while True:
-    status = GPIO.input(pin_rain)
-    if status == True:
-        print("No raindrops were detected, it is not raining")
-    else:
-        print("Raindrops detected, it is raining")
-        time.sleep(0.5)
+# GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+
+# set GPIO direction 
+GPIO.setup(pin_rain, GPIO.IN) #declare it as input
+
+try:
+    while True:
+        status = GPIO.input(pin_rain)
+        if status == 0: # if there is a water present on the board 
+            print("Raindrops detected, it is raining")
+            time.sleep(1.0)
+        else:
+            print("No Raindrops detected, it is not raining")
+            time.sleep(1.0)
+    
+# Reset by pressing CTRL + C
+except KeyboardInterrupt:
+        print("Raindrop detection stopped by the user")
+    
+finally:
+    print("clean up the GPIO")
+    GPIO.cleanup() # cleanup all GPIO
+            
+            
+
