249ba023a9b0a4c9679c0d36ed7fec4f00e94ffd
HisyamJasnin <78057234+HisyamJasnin@users.noreply.github.com>
2021-09-29 22:30:26 +0800

    Update tempsensor.py
    add comment on the codes

--- tempsensor.py
+++ tempsensor.py
@@ -11,24 +11,26 @@ os.system('modprobe w1-therm')
 # define the location for the sensor output is located
 temp_sensor = '/sys/bus/w1/devices/Sensor_name/w1_slave'
 
-# function to read sensor output file
-def read_temp_raw():
+# function to read raw sensor output file
+def raw_temp():
     f = open(temp_sensor, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
 def read_temp():
-    lines = read_temp_raw()
+    lines = raw_temp()
     while lines[0].strip()[-3:] != 'YES':
         sleep(0.2)
-        lines = read_temp_raw()
+        lines = raw_temp()
 
+#  to find the line number for the codes and search t= among the line
     temp_result = lines[1].find('t=')
 
     if temp_result != -1:
+        # to cut all the code and leave the temperature value
         temp_string = lines[1].strip()[temp_result + 2:]
-        # Temperature in Celcius
+        # Temperature in Celcius and to give point on the value
         temp = float(temp_string) / 1000.0
         return temp
 
