b4223ea7d0baf0f09fb31d64fafdbcb35badc676
HisyamJasnin <78057234+HisyamJasnin@users.noreply.github.com>
2021-10-18 13:19:12 +0800

    new changes.. previous are using wrong code

--- tempsensor.py
+++ tempsensor.py
@@ -1,95 +1,84 @@
-# libraries
-import RPi.GPIO as GPIO
+# import libraries
+#!/usr/bin/env python
+import os
 import time
-import mysql.connector
-from time import strftime
 from time import sleep
+from time import strftime
 import datetime
+import mysql.connector
 
-# GPIO Mode (BOARD / BCM)
-GPIO.setmode(GPIO.BCM)
+# Load drivers
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
-# set GPIO Pins
-TRIG = 7
-ECHO = 12
+# define the location for the sensor output is located
+temp_sensor = '/sys/bus/w1/devices/28-3c01f095948b/w1_slave'
 
-# set GPIO direction (IN / OUT)
-print("Distance measurement in progress")
-GPIO.setup(TRIG, GPIO.OUT)  # declare trigger pin as output
-GPIO.setup(ECHO, GPIO.IN)   # declare echo pin as input
-    
-def distance():
-    # set Trigger to HIGH
-    GPIO.output(TRIG, True)
-    
-    # set Trigger after 0.01ms to LOW
-    time.sleep(0.00001)
-    GPIO.output(TRIG, False)
-    
-    StartTime = time.time()
-    StopTime = time.time()
-    
-    # save StartTime
-    while GPIO.input(ECHO) ==0:
-        StartTime = time.time()
-        
-    # save time of arrival
-    while GPIO.input(ECHO) ==1:
-        StopTime = time.time()
-    
-    # time difference between start and arrival
-    TimeElapsed = StopTime - StartTime
-    # multiply with the sonic speed (34300 cm/s)
-    # and divide by 2, because there and back
-    distance = (TimeElapsed * 34300) / 2
-    
-    return distance   
-   
-if __name__ == '__main__':
-    try:
-        while True:
-            dist = distance()
-            print("Distance: %.1f cm " % dist)
-            time.sleep(1)
-            
-            print("clean up")
-            GPIO.cleanup() # cleanup all GPIO
-            break
-            
-         # create connection for MySQL
-        db = mysql.connector.connect(host= "128.199.176.62",
-                                     user= "sam",
-                                     password= "password",
-                                     port= "3306",
-                                     database= "sprinkler")
-    
-        # create time format for the sensor data
-        now = datetime.datetime.now()
-        date = now.strftime('%Y-%m-%d %H:%M:%S')
-         
-        # create cursor object
-        cursor = db.cursor()
-        # get username from database
-        cursor.execute(""" SELECT Username FROM water_level ORDER BY ID DESC LIMIT 1 """)
-        record = cursor.fetchone()
-            
-        # selecting column value into variable
-        username = (record[0])
-            
-        # Excute SQL command and insert data into database
-        cursor.execute(""" UPDATE water_level SET datetime = %s, distance =%s WHERE Username = %s""",(date,str(round(dist,1)),username))
-        # Commit changes in the database
-        db.commit()
+# function to read raw sensor output file
+def raw_temp():
+    f = open(temp_sensor, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
 
-    except mysql.connector.Error as error:
-        print("Failed to get record from database: {}".format(error))
+def read_temp():
+    lines = raw_temp()
+    while lines[0].strip()[-3:] != 'YES':
+        sleep(0.2)
+        lines = raw_temp()
+
+#  to find the line number for the codes and search t= among the line
+    temp_result = lines[1].find('t=')
+
+    if temp_result != -1:
+        # to cut all the code and leave the temperature value
+        temp_string = lines[1].strip()[temp_result + 2:]
+        # Temperature in Celcius and to give point on the value
+        temp = float(temp_string) / 1000.0
+        return temp
+
+# create loop to print out the data every 2 seconds
+while True:
+     print("Current temperature = " + str(read_temp()) + " °C")
+     time.sleep(2)
+     break
      
-    finally:
-        # close cursor and end connection to database
-        if db.is_connected():
-            cursor.close()
-            db.close()
-            print("MySQL connection is closed")
+try:
+    # create connection for MySQL
+    db = mysql.connector.connect(host= "128.199.176.62",
+                                 user= "sam",
+                                 password= "password",
+                                 port= "3306",
+                                 database= "sprinkler")
+    
+    # create time format for the sensor data
+    now = datetime.datetime.now()
+    date = now.strftime('%Y-%m-%d %H:%M:%S')
+         
+    # create cursor object
+    cursor = db.cursor()
+    # get id and username from database
+    cursor.execute("""  SELECT Username FROM tempLog ORDER BY ID DESC LIMIT 1 """)
+    record = cursor.fetchone()
+    
+    # selecting column value into variable
+    username = (record[0])
+    
+    # Excute SQL command and insert data into database
+    cursor.execute(""" UPDATE tempLog SET datetime = %s, temperature = %s WHERE Username = %s""",(date,str(round(read_temp(),1)), username))
+    # cursor.execute(""" INSERT INTO tempLog (Username,datetime,temperature) VALUES (%s,%s,%s) """,(usern,ip,date,str(round(read_temp(),1))))
+    # Commit changes in the database
+    db.commit()
 
-        
-                                                                         
+except mysql.connector.Error as error:
+    print("Failed to get record from database: {}".format(error))
+     
+finally:
+    # close cursor and end connection to database
+    if db.is_connected():
+        cursor.close()
+        db.close()
+        print("MySQL connection is closed")
+
+    
+
