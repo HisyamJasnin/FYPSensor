a9a383595de2d31cb690b0cf638c347e10340357
HisyamJasnin <78057234+HisyamJasnin@users.noreply.github.com>
2021-11-28 16:55:30 +0800

    add some line of code

--- webserver.py
+++ webserver.py
@@ -1,9 +1,25 @@
 from http.server import HTTPServer,BaseHTTPRequestHandler
 import os
+import tempsensor
+import waterlevel
+
+class requestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response_only(200)
+            self.end_headers()
+            tempsensor.main_temp()
+            waterlevel.main_water()
+        elif self.path == "/run":
+            print("Running sprinkler")
 
 def main():
-    PORT = 8080
+    PORT = 9001
     server = HTTPServer(('', PORT), requestHandler)
-    print("Server running on port " + PORT)
+    print("Server running on port " + str(PORT))
     server.serve_forever()
     
+if __name__ == '__main__':
+    main()
+    
+   
